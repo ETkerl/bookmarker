dcbbbd2f417b5dc187df517c26f7dbb87291f590
Frank Etoundi <frank.etoundi@bilendo.de>
2024-04-24 12:16:20 +0200

    Update app/controllers/concerns/renderer.rb

--- app/controllers/concerns/renderer.rb
+++ app/controllers/concerns/renderer.rb
@@ -2,20 +2,11 @@
 
 module Renderer
     def render_object(resource, **options)
-        options.merge!(json: resource, root: :data)
-        options.merge!(status: :ok) unless options.key?(:status)
-        options.merge!(meta: assign_metadata(resource))
-         puts(options)
-        # render json: resource, root: :json, meta: assign_metadata(resource)
+        options[:status] = options.fetch(:status, :ok)
+        options[:root] = options.fetch(:root, :data)
+        options[:meta] = options.fetch(:meta, assign_metadata(resource))
 
-        render options
-        # render({
-        #     json: resource,
-        #     root: :data,
-        #     status: :ok,
-        #     meta: assign_metadata(resource)
-        # }.merge(options))
-        # puts(options)
+        render json: resource, root: :data, **options
     end
 
     def render_errors(errors, status = :unprocessable_entity)
